c06036fffd674a6aae637611acaaf3d2247cecd2
Kizzy <vaibhavmonu8309@gmail.com>
2023-01-29 19:28:23 +0530

    refactor(example): update rpc example code

--- app/src/main/java/kizzy/rpc/MainActivity.kt
+++ app/src/main/java/kizzy/rpc/MainActivity.kt
@@ -1,32 +1,48 @@
 package kizzy.rpc
 
-import android.app.Activity
-import android.widget.EditText
-import android.os.Bundle
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var editText: EditText
     private lateinit var start: Button
     private lateinit var stop: Button
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS,
+                ) != PackageManager.PERMISSION_GRANTED
+            )
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+
 
         editText = findViewById(R.id.token)
         start = findViewById(R.id.start_btn)
         stop = findViewById(R.id.stop_btn)
+
         start.setOnClickListener {
-            token = editText.text.toString()
-            startService(Intent(this, MyService::class.java))
+            startService(Intent(this, MyService::class.java).apply {
+                action = MyService.START_ACTIVITY_ACTION
+                putExtra("TOKEN", editText.text.toString())
+            })
         }
         stop.setOnClickListener { stopService(Intent(this, MyService::class.java)) }
-    }
-
-    companion object {
-        @JvmField
-        var token: String? = null
     }
 }
